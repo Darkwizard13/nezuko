f96fc781ad63ad1b030f152f9d618d14fd06b8a0
Darkwizard13 <98024772+Darkwizard13@users.noreply.github.com>
2022-03-15 23:10:50 +0200

    Update contact.js

--- plugins/contact.js
+++ plugins/contact.js
@@ -6,12 +6,12 @@ const fs = require('fs');
 const Config = require('../config')
 Ktb.addCommand({ pattern: 'git ?(.*)', fromMe: false, desc: 'owner number' }, (async (message, match) => {
 
-//coded by saidali
+//coded by Philomath_zw 
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
-            + 'FN:Amalser [OWNER]\n' // full name
-            + 'ORG:Amalser;\n' // 
-            + 'TEL;type=CELL;type=VOICE;waid=919895828468:+91 8289968468\n' // WhatsApp ID + phone number
+            + 'FN:philomath_zw [OWNER]\n' // full name
+            + 'ORG:Phil;\n' // 
+            + 'TEL;type=CELL;type=VOICE;waid=263714561176 :+263 714561176\n' // WhatsApp ID + phone number
             + 'END:VCARD'
-await message.client.sendMessage(message.jid,{displayname: "Amalser [OWNER]", vcard: vcard}, MessageType.contact)
+await message.client.sendMessage(message.jid,{displayname: "Philomath_zw [OWNER]", vcard: vcard}, MessageType.contact)
 }))
